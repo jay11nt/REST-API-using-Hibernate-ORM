381666fe3577d305378eb93e0524f91dbd01ce34
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-22 13:50:34 +0530

    Update KiaRepository.java

--- Main/Repository/KiaRepository.java
+++ Main/Repository/KiaRepository.java
@@ -1,1 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Kia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+ 
+public interface KiaRepository extends JpaRepository<Kia, Long> {
+}
 
