edd166adbf58e4be930288654e446a8a83945ce5
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-26 09:54:29 +0530

    Update HyundaiRepository.java

--- Main/Repository/HyundaiRepository.java
+++ Main/Repository/HyundaiRepository.java
@@ -4,5 +4,6 @@ import com.example.demo.entity.Hyundai;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface HyundaiRepository extends JpaRepository<Hyundai, Long> {
+public interface HyundaiRepository extends JpaRepository<Hyundai, Long> 
+{
 }
